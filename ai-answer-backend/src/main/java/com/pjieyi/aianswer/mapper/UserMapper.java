302931f838dc2f933cadfe3ad0936b35113a461e
pengjy <pengjy@si-tech.com.cn>
2024-12-23 14:12:19 +0800

    initial  project

---
+++ ai-answer-backend/src/main/java/com/pjieyi/aianswer/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.pjieyi.aianswer.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pjieyi.aianswer.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
