a63a14a226c16a31754f87b6d3c23dd3219270ca
jieyi123 <pjieyi@foxmail.com>
2024-12-26 14:08:09 +0800

    feat(views): 添加管理员相关页面组件

--- ai-answer-frontend/src/router/routes.ts
+++ ai-answer-frontend/src/router/routes.ts
@@ -5,6 +5,17 @@ import UserRegisterPage from "@/views/user/UserRegisterPage.vue";
 import AdminUserPage from "@/views/admin/AdminUserPage.vue";
 import UserLayout from "@/layouts/UserLayout.vue";
 import HomePage from "@/views/HomePage.vue";
+import MyAnswerPage from "@/views/answer/MyAnswerPage.vue";
+import AnswerResultPage from "@/views/answer/AnswerResultPage.vue";
+import DoAnswerPage from "@/views/answer/DoAnswerPage.vue";
+import AppDetailPage from "@/views/app/AppDetailPage.vue";
+import AddScoringResultPage from "@/views/add/AddScoringResultPage.vue";
+import AddQuestionPage from "@/views/add/AddQuestionPage.vue";
+import AddAppPage from "@/views/add/AddAppPage.vue";
+import AdminUserAnswerPage from "@/views/admin/AdminUserAnswerPage.vue";
+import AdminScoringResultPage from "@/views/admin/AdminScoringResultPage.vue";
+import AdminQuestionPage from "@/views/admin/AdminQuestionPage.vue";
+import AdminAppPage from "@/views/admin/AdminAppPage.vue";
 export const routes: Array<RouteRecordRaw> = [
     {
         path: "/",
@@ -12,9 +23,110 @@ export const routes: Array<RouteRecordRaw> = [
         component: HomePage,
     },
     {
+        path: "/add/app",
+        name: "创建应用",
+        component: AddAppPage,
+    },
+    {
+        path: "/add/app/:id",
+        name: "修改应用",
+        props: true,
+        component: AddAppPage,
+        meta: {
+            hideInMenu: true,
+        },
+    },
+    {
+        path: "/add/question/:appId",
+        name: "创建题目",
+        component: AddQuestionPage,
+        props: true,
+        meta: {
+            hideInMenu: true,
+        },
+    },
+    {
+        path: "/add/scoring_result/:appId",
+        name: "创建评分",
+        component: AddScoringResultPage,
+        props: true,
+        meta: {
+            hideInMenu: true,
+        },
+    },
+    {
+        path: "/app/detail/:id",
+        name: "应用详情页",
+        props: true,
+        component: AppDetailPage,
+        meta: {
+            hideInMenu: true,
+        },
+    },
+    {
+        path: "/answer/do/:appId",
+        name: "答题",
+        component: DoAnswerPage,
+        props: true,
+        meta: {
+            hideInMenu: true,
+            access: ACCESS_ENUM.USER,
+        },
+    },
+    {
+        path: "/answer/result/:id",
+        name: "答题结果",
+        component: AnswerResultPage,
+        props: true,
+        meta: {
+            hideInMenu: true,
+            access: ACCESS_ENUM.USER,
+        },
+    },
+    {
+        path: "/answer/my",
+        name: "我的答题",
+        component: MyAnswerPage,
+        meta: {
+            access: ACCESS_ENUM.USER,
+        },
+    },
+    {
         path: "/admin/user",
         name: "用户管理",
         component: AdminUserPage,
+        meta: {
+            access: ACCESS_ENUM.ADMIN,
+        },
+    },
+    {
+        path: "/admin/app",
+        name: "应用管理",
+        component: AdminAppPage,
+        meta: {
+            access: ACCESS_ENUM.ADMIN,
+        },
+    },
+    {
+        path: "/admin/question",
+        name: "题目管理",
+        component: AdminQuestionPage,
+        meta: {
+            access: ACCESS_ENUM.ADMIN,
+        },
+    },
+    {
+        path: "/admin/scoring_result",
+        name: "评分管理",
+        component: AdminScoringResultPage,
+        meta: {
+            access: ACCESS_ENUM.ADMIN,
+        },
+    },
+    {
+        path: "/admin/user_answer",
+        name: "回答管理",
+        component: AdminUserAnswerPage,
         meta: {
             access: ACCESS_ENUM.ADMIN,
         },
--- ai-answer-frontend/src/views/admin/AdminQuestionPage.vue
+++ ai-answer-frontend/src/views/admin/AdminQuestionPage.vue
@@ -5,13 +5,17 @@
       layout="inline"
       @submit="doSearch"
   >
-    <a-form-item field="userName" label="用户名">
-      <a-input v-model="formSearchParams.userName" placeholder="请输入用户名" />
-    </a-form-item>
-    <a-form-item field="userProfile" label="用户简介">
+    <a-form-item field="appId" label="应用 id">
       <a-input
-          v-model="formSearchParams.userProfile"
-          placeholder="请输入用户简介"
+          v-model="formSearchParams.appId"
+          placeholder="请输入应用 id"
+          allow-clear
+      />
+    </a-form-item>
+    <a-form-item field="userId" label="用户 id">
+      <a-input
+          v-model="formSearchParams.userId"
+          placeholder="请输入用户 id"
           allow-clear
       />
     </a-form-item>
@@ -32,8 +36,13 @@
     }"
       @page-change="onPageChange"
   >
-    <template #userAvatar="{ record }">
-      <a-image width="64" :src="record.userAvatar" />
+    <template #questionContent="{ record }">
+      <div
+          v-for="question in JSON.parse(record.questionContent)"
+          :key="question.title"
+      >
+        {{ question }}
+      </div>
     </template>
     <template #createTime="{ record }">
       {{ dayjs(record.createTime).format("YYYY-MM-DD HH:mm:ss") }}
@@ -43,6 +52,9 @@
     </template>
     <template #optional="{ record }">
       <a-space>
+        <a-button type="primary" status="success" @click="doEdit(record)">
+          编辑
+        </a-button>
         <a-button status="danger" @click="doDelete(record)">删除</a-button>
       </a-space>
     </template>
@@ -51,28 +63,30 @@
 <script setup lang="ts">
 import { ref, watchEffect } from "vue";
 import {
-  deleteUserUsingPost,
-  listUserByPageUsingPost,
-} from "@/api/userController";
+  deleteQuestionUsingPost,
+  listQuestionByPageUsingPost,
+} from "@/api/questionController";
 import API from "@/api";
 import message from "@arco-design/web-vue/es/message";
-import {dayjs} from "@arco-design/web-vue/es/_utils/date";
-const formSearchParams = ref<API.UserQueryRequest>({});
+import { dayjs } from "@arco-design/web-vue/es/_utils/date";
+import {useRouter} from "vue-router";
+const router=useRouter();
+const formSearchParams = ref<API.QuestionQueryRequest>({});
 // 初始化搜索条件（不应该被修改）
 const initSearchParams = {
   current: 1,
   pageSize: 10,
 };
-const searchParams = ref<API.UserQueryRequest>({
+const searchParams = ref<API.QuestionQueryRequest>({
   ...initSearchParams,
 });
-const dataList = ref<API.User[]>([]);
+const dataList = ref<API.Question[]>([]);
 const total = ref<number>(0);
 /**
  * 加载数据
  */
 const loadData = async () => {
-  const res = await listUserByPageUsingPost(searchParams.value);
+  const res = await listQuestionByPageUsingPost(searchParams.value);
   if (res.data.code === 0) {
     dataList.value = res.data.data?.records || [];
     total.value = res.data.data?.total || 0;
@@ -89,6 +103,10 @@ const doSearch = () => {
     ...formSearchParams.value,
   };
 };
+
+const doEdit=(question:API.QuestionVO)=>{
+  router.push(`/add/question/${question.appId}`)
+}
 /**
  * 当分页变化时，改变搜索条件，触发数据加载
  * @param page
@@ -103,11 +121,11 @@ const onPageChange = (page: number) => {
  * 删除
  * @param record
  */
-const doDelete = async (record: API.User) => {
+const doDelete = async (record: API.Question) => {
   if (!record.id) {
     return;
   }
-  const res = await deleteUserUsingPost({
+  const res = await deleteQuestionUsingPost({
     id: record.id,
   });
   if (res.data.code === 0) {
@@ -129,25 +147,17 @@ const columns = [
     dataIndex: "id",
   },
   {
-    title: "账号",
-    dataIndex: "userAccount",
+    title: "题目内容",
+    dataIndex: "questionContent",
+    slotName: "questionContent",
   },
   {
-    title: "用户名",
-    dataIndex: "userName",
+    title: "应用 id",
+    dataIndex: "appId",
   },
   {
-    title: "用户头像",
-    dataIndex: "userAvatar",
-    slotName: "userAvatar",
-  },
-  {
-    title: "用户简介",
-    dataIndex: "userProfile",
-  },
-  {
-    title: "权限",
-    dataIndex: "userRole",
+    title: "用户 id",
+    dataIndex: "userId",
   },
   {
     title: "创建时间",
